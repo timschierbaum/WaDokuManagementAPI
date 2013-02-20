d63227e048c55b7d7d7de21495a047b1cdea03b8
Tim Schierbaum <tim.schierbaum@gmail.com>
2013-02-20 11:40:00 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wadokumanagementapi::Application.config.secret_token = '65f9e299275af8e43ec431d891fc89fab5ca4e2b5b3e8c08e5038a6724438d07d0014b92a2521551059f5e7771ec58a39f9d31757c53edeefba247bddd8ce00d'
