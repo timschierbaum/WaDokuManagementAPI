422ed45662fb0a65d35840666350498ad469b001
thebluber <thebluber@googlemail.com>
2013-02-20 16:16:24 +0100

    add a admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+ User.create!(:name => 'Admin', :password => 'password', :password_confirmation => 'password', :email => 'admin@example.com', :status => 'admin')
