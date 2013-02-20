eee1d71cf78ef9a92a0f15577f8c17329d1c5283
Tim Schierbaum <tim.schierbaum@gmail.com>
2013-02-20 13:30:13 +0100

    add user model

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "users/show" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :name => "Name",
+      :email => "Email",
+      :status => "Status",
+      :password => "Password"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Name/)
+    rendered.should match(/Email/)
+    rendered.should match(/Status/)
+    rendered.should match(/Password/)
+  end
+end
